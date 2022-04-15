92db31c9ce5767a47cf182029384bedf871225d6
Harsh Mangalam <hmangalam@oplglobal.com>
2022-04-15 17:50:54 +0530

    variables

---
+++ data-types.js
@@ -0,0 +1,59 @@
+// Number
+
+// let followers = 3423
+
+// let jeeMarks = -20
+
+// let pi = 3.14
+
+// let stars = Infinity
+// let negativeYAxis = -Infinity
+
+// NaN --- > Not a Number
+
+// console.log(followers)
+// console.log(jeeMarks)
+// console.log(pi)
+
+// let a = "100"
+
+// let aNumber = Number(a)
+
+// let fullName = NaN
+
+// console.log(stars)
+// console.log(negativeYAxis)
+// console.log(fullName)
+
+// console.log(aNumber)
+
+// let a = "!"
+
+// string ----> number
+
+// let b = Number(a)
+
+// console.log(a)
+// console.log(b)
+
+// console.log(typeof a)
+// console.log(typeof b)
+
+let a = 20;
+let b = 30;
+
+// add two number
+
+console.log(a + b);
+
+console.log(a / b);
+
+let largeNumber = 646545455454545545454n
+
+
+let phoneNumber = "+91636353553434"
+
+
+// let isEmailAddressVerified = false
+
+let isEmailAddressVerified = true
---
+++ variable.js
@@ -0,0 +1,8 @@
+// let name = "Harsh"
+// let rollNo = 41
+
+// // rule 1
+
+
+// let name = ""
+// let
