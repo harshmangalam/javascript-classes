ef5efeb18243ec601eeda54db9e3f2dd8c0ad33f
Harsh Mangalam <hmangalam@oplglobal.com>
2022-04-16 22:05:01 +0530

    operators

---
+++ operator.js
@@ -0,0 +1,38 @@
+let positiveNumber = 5;
+
+let negativeNumber = -positiveNumber;
+
+// console.log(positiveNumber)
+// console.log(negativeNumber)
+
+// +, -, **, % , /
+
+const shirt = 2 * 160;
+
+const trowser = 4 * 100;
+
+const discount = ((shirt + trowser) * 3) / 100;
+
+// console.log(discount);
+
+// console.log(2 ** 3);
+// console.log(2 * 2 * 2);
+
+// increment and decrement operator
+
+
+let counter = 0;
+
+console.log(counter) // 0
+
+// counter = counter + 1; // 0 + 1 = 1
+
+counter++
+
+console.log(counter) // 1
+
+// counter = counter -1 // 0
+
+counter--
+
+console.log(counter) // 0
