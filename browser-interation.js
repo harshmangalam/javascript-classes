ec83831777d588f2b57fe85a3eaa30330fe3f7fa
Harsh Mangalam <hmangalam@oplglobal.com>
2022-04-16 20:57:30 +0530

    browser-intraction

---
+++ browser-interation.js
@@ -0,0 +1,14 @@
+// console.log("working...")
+
+// alert
+
+// alert("Your browser have many spam and virus !");
+
+// alert(true)
+
+
+let isLearning = confirm("Are you learning javascript ?")
+
+console.log(isLearning)
+
+
