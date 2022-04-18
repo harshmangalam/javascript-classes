0f0d665df45387468bb13d38ea88da0bae30d663
Harsh Mangalam <hmangalam@oplglobal.com>
2022-04-18 20:32:56 +0530

    conditional

---
+++ comparison.js
@@ -0,0 +1,57 @@
+
+// Number comparison 
+
+let mathMarks = "80"
+
+let scienceMarks = 85
+
+
+let sstMarks = 80
+
+// mathMarks === scienceMarks  
+// // console.log(mathMarks > scienceMarks)
+
+// console.log(mathMarks === sstMarks) 
+// console.log(mathMarks == sstMarks)
+
+
+
+
+
+// let name1 = "Aanand"
+
+// let name2 = "Avish"
+
+
+
+
+// console.log(name2 > name1)
+
+
+let a = 1
+let b  = 0
+
+//  true or false 
+
+
+// console.log(true === 0)
+// console.log(true == 0)
+
+
+
+// null and undefined data type 
+
+
+const user = null
+
+let cart
+
+
+
+
+
+console.log(cart === user) // false
+console.log(cart == user) // true
+
+
+
