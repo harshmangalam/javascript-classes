d6c2502e0c4d245f05b0e2067a2d989ebea2c757
Harsh Mangalam <hmangalam@oplglobal.com>
2022-04-18 21:15:29 +0530

    logical operator

---
+++ logical-operator.js
@@ -0,0 +1,63 @@
+// || , && , !
+
+// if(true || true){
+// console.log("i am working...")
+// }
+
+// if (true || false) {
+//   console.log("i am working...");
+// }
+
+// if (false || false) {
+//   console.log("i am working...");
+// }else {
+//     console.log("else is working...")
+// }
+
+
+// if(true && true){
+// console.log("i am working...")
+// }
+
+// if (true && false) {
+//   console.log("i am working...");
+// }else{
+
+// }
+
+// if (false && false) {
+//   console.log("i am working...");
+// }else {
+//     console.log("else is working...")
+// }
+
+
+
+// if(!true){
+//     console.log("hiii")
+// }
+
+
+
+// if(!false){
+//     console.log("hiiii")
+// }
+
+
+// authentication 
+
+
+
+// user
+
+const password = getting from browser
+
+const user = getUser()
+
+if(!user){
+    console.log("create your account")
+}
+
+if(password !== user.password){
+    console.log("incorret password")
+}
