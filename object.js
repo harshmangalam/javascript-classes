b80c1145111038a16672b4fe709330cf1d1226d2
Harsh Mangalam <hmangalam@oplglobal.com>
2022-04-22 21:28:40 +0530

    object basics

---
+++ object.js
@@ -0,0 +1,89 @@
+// let firsName = "first";
+// let lastName = "last";
+// let isLoggedin = false;
+
+// let firstName = prompt("Please enter first name", "first");
+
+// let user = {
+//   firstName: "test",
+//   lastName: "last",
+//   isLoggedin: true,
+//   profilePic: "https://unsplash.com/jdhdgtydggdtygbygydghyud",
+//   age: 30,
+//   9: "jjjj",
+// };
+
+// if (user.profilePic) {
+//   console.log("profile pic is available");
+// } else {
+//   console.log("profile pic is not available");
+// }
+
+// if ("profilePic" in user) {
+//   console.log("profile pic is available");
+// } else {
+//   console.log("profile pic is not available");
+// }
+
+// console.log(user);
+// console.log(user.age);
+
+// console.log(typeof user[9]);
+
+// if you have valid variable name as key then you can use dot
+
+// let if = "46"
+
+// console.log(if)
+
+// const object1 = {
+//   if: 12,
+//   else: 56,
+//   for: 56,
+//   let: "jdd",
+// };
+
+// console.log(typeof object1.if)
+
+// user.profilePic
+//   ? console.log("profile pic is avai.....")
+//   : console.log("not available ");
+
+// let user = {
+//   firstName: "test",
+//   lastName: "last",
+//   isLoggedin: true,
+//   profilePic: "https://unsplash.com/jdhdgtydggdtygbygydghyud",
+//   age: 30,
+//   9: "jjjj",
+
+// };
+
+// for in loop
+
+// for (let key in user) {
+//     console.log(key)
+//     console.log(user[key])
+// }
+
+// let a = {
+//   key1: "value1",
+//   key2: 56,
+// };
+
+// let b = Object.assign({}, a);
+
+// a.key3 = "value3";
+
+// console.log(a);
+
+// console.log(b);
+
+
+const user = {name:""}
+const admin = {name:""}
+
+
+console.log(user === admin)
+
+
