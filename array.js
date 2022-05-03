23a90eb8d3c4d0840320b1e74a60935755b57bbf
Harsh Mangalam <hmangalam@oplglobal.com>
2022-05-03 10:05:59 +0530

    garbage collection

---
+++ array.js
@@ -0,0 +1,41 @@
+// const numbers = [1, 2, 3, 5, 5, 6, 7, 8, 9];
+
+// const array = ["email", 34, true, null, "ddd", "454r4rt45"];
+
+// console.log(numbers);
+// console.log(array[2])
+
+// console.log(numbers.length)
+
+
+
+// for (let i = 0; i < array.length; i++) {
+//   console.log(array[i]);
+// }
+
+
+
+
+
+
+
+
+const counter = []
+
+// counter[0] = 1
+// counter[1] = 2
+// counter[2] = 3
+
+
+// push() method 
+
+
+// counter.push(1)
+// counter.push(2)
+// counter.push(3)
+
+// counter.push(1,2,3)
+
+
+
+console.log(counter)
---
+++ garbage-collection.js
@@ -0,0 +1,25 @@
+const profile = {
+  profilePic: "unsplash.com/wiyd732tr67t2t6t632tt36td3",
+  isActive: true,
+  age: 50,
+};
+
+
+
+
+profile.email = "some@gmail.com"
+
+let profile1 = {...profile}
+
+
+
+
+
+profile1 = null
+
+
+
+
+
+
+getProfileInformation()
