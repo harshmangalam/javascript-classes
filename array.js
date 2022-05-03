3d2c4cb4964932f32ccb0f73a409558e7d7869e4
Harsh Mangalam <hmangalam@oplglobal.com>
2022-05-03 21:07:00 +0530

    array

--- array.js
+++ array.js
@@ -7,28 +7,17 @@
 
 // console.log(numbers.length)
 
-
-
 // for (let i = 0; i < array.length; i++) {
 //   console.log(array[i]);
 // }
 
-
-
-
-
-
-
-
-const counter = []
+// const counter = []
 
 // counter[0] = 1
 // counter[1] = 2
 // counter[2] = 3
 
-
-// push() method 
-
+// push() method
 
 // counter.push(1)
 // counter.push(2)
@@ -36,6 +25,97 @@ const counter = []
 
 // counter.push(1,2,3)
 
+// console.log(counter)
 
+// const count = [5,4]
 
-console.log(counter)
+// count.push(5)
+// count.push(4)
+
+// const users = [];
+
+// const rohan = {
+//   userName: "rohan",
+//   password: "rohan12345",
+// };
+
+// users.push(rohan);
+
+// const shyam = {
+//   userName: "shyam",
+//   password: "shy123",
+// };
+
+// users.push(shyam);
+
+// users.push({
+//   userName: "harsh",
+//   password: "123456",
+// });
+
+// console.log(users);
+
+// for (let i = 0; i < users.length; i++) {
+//   console.log(users[i].userName);
+//   console.log(users[i].password);
+// }
+
+// pop() method in array
+
+// const removeLastUser = users.pop()
+
+// console.log(users)
+// console.log(removeLastUser)
+
+// console.log(users)
+
+// console.log(users.shift())
+
+// console.log(users)
+
+// users.unshift(9)
+
+// console.log(users)
+
+// push()
+// pop()
+// shift()
+// unshift()
+
+// const users = [
+//   {
+//     id: 1,
+//     name: "name1",
+//     age: 23,
+//   },
+//   {
+//     id: 2,
+//     name: "name2",
+//     age: 20,
+//   },
+// ];
+
+// id        name        age
+// 1         name1       23
+// 2         name2       20
+
+// const numbers = [1,2,4,6,56,89]
+
+// numbers.shift()
+// numbers.pop()
+
+// splice()
+
+// numbers.splice(2,1)
+
+// numbers.splice(4,1)
+
+// console.log(numbers)
+
+// forEach loop
+
+const numbers = [1, 2, 3, 4];
+
+numbers.forEach((n, i) => {
+  console.log(n + ":::::::::" + i);
+});
