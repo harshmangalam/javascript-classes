13461a39fd35cd40d26a1f0368de8e83a907951c
Harsh Mangalam <hmangalam@oplglobal.com>
2022-05-04 20:54:55 +0530

    numbers

---
+++ numbers.js
@@ -0,0 +1,68 @@
+// representation
+
+let rank = 20023;
+
+let money = 1e15;
+
+
+let bitCointToRupees = 1e-15
+
+
+
+// decimal 
+
+
+
+// const data = 45.777
+
+
+
+
+// let number = "25.90"
+
+
+
+// console.log(number.toString())
+
+
+let data = 34.678
+
+
+console.log(Math.floor(data))
+
+console.log(Math.ceil(data))
+
+
+console.log(Math.round(data))
+
+
+console.log(Math.trunc(data))
+
+
+
+const price = 56.78907890
+
+
+console.log(price.toFixed(3))
+
+
+
+const negative = -345
+
+const convert = Math.abs(negative)
+
+console.log(convert)
+
+
+
+// console.log(Math)
+
+
+// console.log(Math.min(1,24,67,90))
+
+
+
+// const value = -Infinity
+
+
+// console.log(isFinite(value))
---
+++ object-methods.js
@@ -0,0 +1,33 @@
+const product = {
+  productName: "Laptop",
+  quantity: 1,
+  price: 30_000,
+  1: 8484,
+};
+
+// Object.keys()
+
+const productKeys = Object.keys(product);
+
+console.log(productKeys);
+
+
+
+// Object.values()
+
+
+const productValue = Object.values(product)
+
+console.log(productValue)
+
+
+
+
+// Object.entries()
+
+
+const productEntries = Object.entries(product)
+console.log(productEntries)
+
+
+
