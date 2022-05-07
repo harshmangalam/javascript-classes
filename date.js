ba0aa62584291bf43e7f49a1aa002104c94969bf
Harsh Mangalam <harshdev8218@gmail.com>
2022-05-07 23:02:36 +0530

    date and time

---
+++ date.js
@@ -0,0 +1,67 @@
+// const date =  new Date()
+
+// console.log(date)
+
+
+
+// const date = new Date(2017, 2, 14, 4, 50)
+
+
+// console.log(date)
+
+
+// const date = new Date()
+
+// const fullYear = date.getFullYear()
+
+// console.log(fullYear)
+
+
+// console.log(date.getMonth())
+
+// const date = new Date()
+
+// date.getFullYear()
+
+// date.getMonth()
+
+// date.getHours()
+
+
+
+// console.log(date.getTime())
+
+
+
+
+// function Date() {
+
+//     // const this = {}
+
+//     this.fullYear = () => {
+
+//     }
+
+
+//     // return this
+// }
+
+
+
+
+// const date = new Date()
+
+// date.getFullYear()
+
+
+
+
+const date = Date.now()
+
+console.log(date)
+
+
+
+
+
+const number = [1,2,3,4,5,6,7]
