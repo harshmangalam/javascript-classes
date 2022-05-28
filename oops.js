0b5c5cc40a8c6d040904998b90ffbe8b676ee515
Harsh Mangalam <hmangalam@oplglobal.com>
2022-05-28 19:59:05 +0530

    oops

---
+++ oops.js
@@ -0,0 +1,38 @@
+// function User(){
+//     this.firstName = "harsh"
+// }
+
+// const harsh = new User()
+
+// console.log(harsh.firstName)
+
+// class User {
+//    isAdmin = false;
+
+//   constructor(firstName, lastName) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//  this }
+
+//   fullName() {
+//     return this.firstName + " " + this.lastName;
+//   }
+// }
+
+// const harsh = new User("harsh", "mangalam");
+
+// const ram = new User("ram","k")
+
+// // console.log(harsh.fullName());
+
+// console.log(harsh.isAdmin)
+// console.log(ram.isAdmin)
+
+// const Admin = class {
+//   isAdmin = true;
+// };
+
+// const harsh = new Admin()
+
+// console.log(harsh.isAdmin)
+
